a74cc45127ec2e7ac6ed2872d424dce375b30f55
josephdiniso <josephdiniso@gmail.com>
2021-11-16 15:01:39 -0500

    Add better thresholding and mask generation
    Makes better contour detection, next step is to perform collision
    detection for the contours and make a sound when a collision occurs.

--- src/vision/main.py
+++ src/vision/main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from typing import Tuple
+from typing import Tuple, List
 
 import numpy as np
 import imutils
@@ -22,33 +22,34 @@ class Detector:
     def __init__(self):
         self.height = 700
         self.width = 700
-        self.timer_count = 3
+        self.timer_count = 1
         self.frame = None
         self.gray = None
-        self.prev_frame = None
+        self.frame_queue = []
         self.left_color = None
         self.right_color = None
         self.time_elapsed = 0
         self.state = States.INIT
         self.start_time = 0
-        self.color_coords = [(int(self.width * 0.3), int(self.height * 0.3)),
-                             (int(self.width * 0.7), int(self.height * 0.3)),
-                             (int(self.width * 0.3), int(self.height * 0.7)),
-                             (int(self.width * 0.7), int(self.height * 0.7))]
+        self.circle_coordinates = [(int(self.width * 0.3), int(self.height * 0.3)),
+                                   (int(self.width * 0.7), int(self.height * 0.3)),
+                                   (int(self.width * 0.3), int(self.height * 0.7)),
+                                   (int(self.width * 0.7), int(self.height * 0.7))]
         self.colors = []
 
-    def get_color(self):
+        self.webcam()
+
+    def get_color(self) -> None:
         """
-        Gets the average of the colors inside of the bounding circles and returns the left and right average color
-        Args:
-    
+        gets the average of the colors within the bounding circles respectively
+
         Returns:
             None
         """
         if self.state == States.DONE_1:
-            coords = self.color_coords[:2]
+            coords = self.circle_coordinates[:2]
         elif self.state == States.DONE_2:
-            coords = self.color_coords[2:]
+            coords = self.circle_coordinates[2:]
         else:
             coords = []
         for coord in coords:
@@ -58,52 +59,53 @@ class Detector:
     def prompt_color(self) -> np.array:
         """
         Draws the prompt for color selection on the frame and returns it
-        Args:
     
         Returns:
-    
+            Modified frame with prompt
         """
         font = cv2.FONT_HERSHEY_SIMPLEX
         frame = self.frame.copy()
-        org = (int(self.width * 0.228), int(self.height * 0.085714))
+        origin = (int(self.width * 0.228), int(self.height * 0.085714))
         font_scale = 1.0
         txt_color = (255, 255, 255)
         txt_thickness = 2
         radius = int(self.width * 0.03)
         color = (255, 255, 255)
         thickness = 2
+        # Displays color fill circles and prompts user to calibrate
         if self.state in (States.INIT, States.COUNTDOWN_1, States.DONE_1, States.COUNTDOWN_2):
-            cv2.putText(frame, "Press Space When Ready", org, font, font_scale, (0, 0, 0), thickness=txt_thickness * 2)
-            cv2.putText(frame, "Press Space When Ready", org, font, font_scale, txt_color, thickness=txt_thickness)
+            cv2.putText(frame, "Press Space When Ready", origin, font, font_scale, (0, 0, 0),
+                        thickness=txt_thickness * 2)
+            cv2.putText(frame, "Press Space When Ready", origin, font, font_scale, txt_color, thickness=txt_thickness)
 
-            for coord in self.color_coords:
+            for coord in self.circle_coordinates:
                 cv2.circle(frame, coord, radius, color, thickness)
                 cv2.circle(frame, coord, radius, color, thickness)
             if self.state in (States.DONE_1, States.COUNTDOWN_2, States.DONE_2):
-                for track_color, coord in zip(self.colors, self.color_coords):
+                for track_color, coord in zip(self.colors, self.circle_coordinates):
                     cv2.circle(frame, tuple(coord), radius - 1, track_color, -1)
             num_coord = (int(self.width * 0.5) - 10, int(self.height * 0.5) - 5)
             if self.state in (States.COUNTDOWN_1, States.COUNTDOWN_2):
                 num_scale = 2.0
                 cv2.putText(frame, f"{self.timer_count - self.time_elapsed}", num_coord, font, num_scale, txt_color,
                             thickness=txt_thickness)
+        # Prompts user to confirm the colors
         elif self.state == States.DONE_2:
             prompt = "Press Space to Confirm, Esc to Redo"
-            org = (int(self.width * 0.19), int(self.height * 0.085714))
-            cv2.putText(frame, prompt, org, font, 0.8, (0, 0, 0), thickness=4)
-            cv2.putText(frame, prompt, org, font, 0.8, txt_color, thickness=2)
-            for coord in self.color_coords:
+            origin = (int(self.width * 0.19), int(self.height * 0.085714))
+            cv2.putText(frame, prompt, origin, font, 0.8, (0, 0, 0), thickness=4)
+            cv2.putText(frame, prompt, origin, font, 0.8, txt_color, thickness=2)
+            for coord in self.circle_coordinates:
                 cv2.circle(frame, coord, radius, color, thickness)
                 cv2.circle(frame, coord, radius, color, thickness)
             if self.state in (States.DONE_1, States.COUNTDOWN_2, States.DONE_2):
-                for track_color, coord in zip(self.colors, self.color_coords):
+                for track_color, coord in zip(self.colors, self.circle_coordinates):
                     cv2.circle(frame, tuple(coord), radius - 1, track_color, -1)
         return frame
 
     def impose_blocks(self) -> np.array:
         """
         Draws bounding blocks on the bottom of the screen as different drum components
-        Args:
 
         Returns:
             Output frame
@@ -123,20 +125,42 @@ class Detector:
         return frame
 
     def generate_mask(self, gray: np.array) -> np.array:
-        delta = cv2.absdiff(self.prev_frame, gray)
-        thresh = cv2.threshold(delta, 25, 255, cv2.THRESH_BINARY)[1]
-        thresh = cv2.dilate(thresh, None, iterations=2)
-        thresh = cv2.bitwise_and(self.frame, self.frame, mask=thresh)
+        """
+        Uses motion estimation then color thresholding to create a mask to be used for contour detection
+        Args:
+            gray: Grayscale image used for calculating the motion mask
 
-        buffer = 5
-        master_mask = np.zeros(thresh.shape[:2], dtype="uint8")
-        for color in self.colors:
-            mask = cv2.inRange(thresh, (color - buffer).astype(int), (color + buffer).astype(int))
-            mask = cv2.dilate(mask, None, iterations=2)
-            master_mask = cv2.bitwise_or(mask, master_mask)
-        return master_mask
+        Returns:
+            Output mask
+        """
+        if self.frame_queue:
+            # Motion estimation
+            delta = cv2.absdiff(self.frame_queue[0], gray)
+            thresh = cv2.threshold(delta, 25, 255, cv2.THRESH_BINARY)[1]
+            thresh = cv2.dilate(thresh, None, iterations=2)
+            thresh = cv2.bitwise_and(self.frame, self.frame, mask=thresh)
+            # Color range
+            buffer = 5
+            master_mask = np.zeros(thresh.shape[:2], dtype="uint8")
+            # Color thresholding
+            for color in self.colors:
+                mask = cv2.inRange(thresh, (color - buffer).astype(int), (color + buffer).astype(int))
+                mask = cv2.dilate(mask, None, iterations=5)
+                master_mask = cv2.bitwise_or(mask, master_mask)
+            # Fills the mask for easier detection
+            rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 30))
+            master_mask = cv2.morphologyEx(master_mask, cv2.MORPH_CLOSE, rect_kernel)
+            return master_mask
+        else:
+            return np.zeros((self.width, self.height), dtype="uint8")
 
-    def manage_thresholding(self):
+    def manage_thresholding(self) -> None:
+        """
+        Modifies the state of the system based on time elapsed and the current state
+
+        Returns:
+            None
+        """
         self.time_elapsed = int(time.time() - self.start_time)
         if self.time_elapsed >= self.timer_count:
             if self.state == States.COUNTDOWN_1:
@@ -145,15 +169,41 @@ class Detector:
                 self.state = States.DONE_2
             self.get_color()
 
-    def webcam(self):
+    def generate_contours(self, gray: np.array) -> List:
+        """
+        Generates a masked image with the generate_mask method and then computes the contours based on that and returns
+        it
+
+        Args:
+            gray: Grayscale 'raw' image
+
+        Returns:
+            list of contours
+        """
+        mask = self.generate_mask(gray)
+        contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+        contours = imutils.grab_contours(contours)
+        drawn_contours = self.frame.copy()
+        for c in contours:
+            if cv2.contourArea(c) < 500:
+                continue
+            (x, y, w, h) = cv2.boundingRect(c)
+            cv2.rectangle(drawn_contours, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        # cv2.imshow("mask", mask)
+        # cv2.imshow("Delta", drawn_contours)
+        return contours
+
+    def webcam(self) -> None:
+        """
+        Main driver of the program, works out the timing and user inputs as well as calling the necessary
+        methods.
+
+        Returns:
+            None
+        """
         cap = cv2.VideoCapture(0)
         if not cap.isOpened():
             raise IOError("Cannot open webcam")
-        _, prev_frame = cap.read()
-        prev_frame = cv2.cvtColor(prev_frame, cv2.COLOR_BGR2GRAY)
-        prev_frame = cv2.resize(prev_frame, (700, 700))
-        self.prev_frame = cv2.flip(prev_frame, 1)
-
         while True:
             if self.state in (States.COUNTDOWN_1, States.COUNTDOWN_2):
                 self.manage_thresholding()
@@ -161,24 +211,18 @@ class Detector:
             frame = cv2.resize(frame, (700, 700))
             self.frame = cv2.flip(frame, 1)
             processed = self.impose_blocks()
+            # Grayscale converted image of 'raw' image
             gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
-
+            # If color is confirmed, perform masking and contour detection
             if self.state == States.CONFIRM:
-                mask = self.generate_mask(gray)
-                contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                contours = imutils.grab_contours(contours)
-                for c in contours:
-                    if cv2.contourArea(c) < 1000:
-                        continue
-                    (x, y, w, h) = cv2.boundingRect(c)
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.imshow("Delta", mask)
+                contours = self.generate_contours(gray)
+            # Otherwise prompt the user to calibrate
             else:
                 processed = self.prompt_color()
-
             c = cv2.waitKey(1)
             if c == ord("q"):
                 break
+            # If space is pressed and the user is prompted to calibrate
             elif c == ord(" ") and self.state not in (
                     States.COUNTDOWN_1, States.COUNTDOWN_2, States.CONFIRM, States.DONE_2):
                 if self.state == States.INIT:
@@ -186,12 +230,17 @@ class Detector:
                 elif self.state == States.DONE_1:
                     self.state = States.COUNTDOWN_2
                 self.start_time = time.time()
+            # If space is pressed and the user is prompted to confirm
             elif c == ord(" ") and self.state == States.DONE_2:
                 self.state = States.CONFIRM
+            # If escape is pressed and the user is prompted to confirm
             elif c == 27 and self.state == States.DONE_2:
                 self.state = States.INIT
                 self.colors.clear()
-            self.prev_frame = gray
+            # Populates frame queue for motion estimation
+            self.frame_queue.append(gray.copy())
+            if len(self.frame_queue) > 10:
+                self.frame_queue.pop(0)
             cv2.imshow('Input', processed)
 
         cap.release()
@@ -200,7 +249,6 @@ class Detector:
 
 def main():
     detector = Detector()
-    detector.webcam()
 
 
 if __name__ == "__main__":
