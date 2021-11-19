cf9b39e4164402479889e3434757509e4ecfccfa
josephdiniso <josephdiniso@gmail.com>
2021-11-18 19:26:52 -0500

    Add docstring comments
    Finished product, everything seems to work decently.
    The color thresholding can be improved, I just don't know how right
    now to do that.

--- src/vision/main.py
+++ src/vision/main.py
@@ -1,55 +1,125 @@
 #!/usr/bin/env python3
-from typing import Tuple, List
-
-import numpy as np
-import imutils
-import cv2
-import playsound
-
+import os
+from typing import List
 import time
 from enum import Enum
 import pickle
 import argparse
+import threading
+
+import numpy as np
+import imutils
+import cv2
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 class States(Enum):
+    """
+    Enum class to facilitate program FSM
+    """
     INIT = 0
     COUNTDOWN_1 = 1
     DONE_1 = 2
-    COUNTDOWN_2 = 3
-    DONE_2 = 4
-    CONFIRM = 5
+    CONFIRM_1 = 3
+    COUNTDOWN_2 = 4
+    DONE_2 = 5
+    CONFIRM_2 = 6
 
 
 class CollisionBox:
-    def __init__(self, name: str, song_file: str, color: np.array):
+    """
+    Collision box object made to be used in composition with the Detector class
+    """
+
+    def __init__(self, song_file: str, color: np.array, bias: float = 0):
+        """
+        Initializes collision box
+        Args:
+            song_file: Audio file name
+            color: Respective color of collision box
+            bias: Sound bias in decibels to control the volume in post
+        """
         self.last_collision = 0
-        self.name = name
-        self.song_file = song_file
+        self.song = AudioSegment.from_mp3(song_file) + bias
         self.color = np.array(color)
+        self.velocity_timer = 0
 
     def has_collision(self) -> None:
         """
-        Checks if enough time has passed to play a sound
+        Checks if enough time has expired to play sound
+
+        Calculates the velocity of the stick during contact and adjusts the volume and pitch accordingly. Then plays
+        the modified sound in a new thread to allow for non-blocking sound.
 
         Returns:
             None
         """
         if float(time.time() - self.last_collision) > 0.5:
-            print(self.name)
+            velocity = 56 / (time.time() - self.velocity_timer)
+            # Volume calculation
+            scale = round(velocity / 2000 * 25)
+            scale = min(25, scale)
+            scale = max(12, scale)
+
+            # Pitch calculation
+            octaves = round(velocity / 1400 * 1)
+            octaves = min(1, octaves)
+            octaves = max(0, octaves)
+            octaves -= 0.5
+            new_sample_rate = int(self.song.frame_rate * (2.0 ** octaves))
+            pitch_modulated = self.song._spawn(self.song.raw_data, overrides={'frame_rate': new_sample_rate})
+
             self.last_collision = time.time()
-            playsound.playsound(self.song_file, False)
+            t = threading.Thread(target=play, args=(pitch_modulated + scale,))
+            t.start()
 
     def reset_collision(self) -> None:
+        """
+        Sets the collision timer such that the next collision will play a sound.
+
+        Is generally called when the stick has been detected *above* a certain threshold on the screen, signifying that
+        the stick has reset itself.
+
+        Returns:
+            None
+        """
         self.last_collision = time.time() - 1
+
+    def start_timer(self) -> None:
+        """
+        Starts a timeout timer to avoid repeated hits.
+
+        Returns:
+            None
+        """
+        self.velocity_timer = time.time()
 
 
 class Detector:
+    """Drum detector and simulator
+
+    Facilitates all of the computation behind tracking the drumsticks and calling the necessary functions to play sounds
+    when a collision is detected.
+
+    """
+
     def __init__(self, prev: bool):
+        """
+        Initializes Detector class
+
+        Args:
+            prev: Determines whether to use the previously detected color from the last run
+        """
         self.height = 700
         self.width = 700
+        # Seconds to wait for calibration
         self.timer_count = 2
+        # Frame to be used for vision techniques
         self.frame = None
+        # Frame to be shown to the user
+        self.processed = None
+        # Grayscale frame used in queue for motion detection
         self.gray = None
         self.frame_queue = []
         self.time_elapsed = 0
@@ -58,34 +128,40 @@ class Detector:
         if not prev:
             self.state = States.INIT
         else:
-            self.state = States.CONFIRM
+            self.state = States.CONFIRM_2
             with open("colors.pickle", "rb") as f:
                 self.colors = pickle.load(f)
-        self.circle_coordinates = [(int(self.width * 0.3), int(self.height * 0.3)),
-                                   (int(self.width * 0.7), int(self.height * 0.3)),
-                                   (int(self.width * 0.3), int(self.height * 0.7)),
-                                   (int(self.width * 0.7), int(self.height * 0.7))]
-
-        self.height_thresh = self.height * 0.8
-
-        self.boxes = [CollisionBox("one", "drum1.mp3", (255, 0, 0)), CollisionBox("two", "drum2.mp3", (0, 255, 0)),
-                      CollisionBox("three", "drum3.mp3", (0, 0, 255)), CollisionBox("four", "drum4.mp3", (255, 0, 255))]
+        self.circle_coordinates = [(int(self.width * 0.1), int(self.height * 0.93)),
+                                   (int(self.width * 0.93), int(self.height * 0.93)),
+                                   (int(self.width * 0.35), int(self.height * 0.93)),
+                                   (int(self.width * 0.62), int(self.height * 0.93))]
+        self.speed_thresh = self.height * 0.75
+        self.reset_thresh = self.height * 0.78
+        self.height_thresh = self.height * 0.83
+        self.distance = self.height_thresh - self.speed_thresh
+        prefix_dir = "../../resources/sounds/"
+        self.boxes = [CollisionBox(os.path.join(prefix_dir, "drum1.mp3"), (255, 0, 0)),
+                      CollisionBox(os.path.join(prefix_dir, "drum3.mp3"), (0, 255, 0), bias=5),
+                      CollisionBox(os.path.join(prefix_dir, "drum2.mp3"), (0, 0, 255), bias=3),
+                      CollisionBox(os.path.join(prefix_dir, "drum4.mp3"), (255, 0, 255), bias=5)]
         self.webcam()
 
-    def get_color(self) -> None:
+    def measure_color(self) -> None:
         """
-        gets the average of the colors within the bounding circles respectively
+        Measures the average color in each respective detection circle
+
+        Takes the average of a 4x4 square inside the circle for color detection to be used later.
 
         Returns:
             None
         """
         if self.state == States.DONE_1:
-            coords = self.circle_coordinates[:2]
+            coordinates = self.circle_coordinates[:2]
         elif self.state == States.DONE_2:
-            coords = self.circle_coordinates[2:]
+            coordinates = self.circle_coordinates[2:]
         else:
-            coords = []
-        for coord in coords:
+            coordinates = []
+        for coord in coordinates:
             avg = np.average(self.frame[coord[1] - 2:coord[1] + 2, coord[0] - 2:coord[0] + 2], axis=(0, 1))
             self.colors.append(avg)
 
@@ -106,11 +182,10 @@ class Detector:
         color = (255, 255, 255)
         thickness = 2
         # Displays color fill circles and prompts user to calibrate
-        if self.state in (States.INIT, States.COUNTDOWN_1, States.DONE_1, States.COUNTDOWN_2):
+        if self.state in (States.INIT, States.COUNTDOWN_1, States.COUNTDOWN_2):
             cv2.putText(frame, "Press Space When Ready", origin, font, font_scale, (0, 0, 0),
                         thickness=txt_thickness * 2)
             cv2.putText(frame, "Press Space When Ready", origin, font, font_scale, txt_color, thickness=txt_thickness)
-
             for coord in self.circle_coordinates:
                 cv2.circle(frame, coord, radius, color, thickness)
                 cv2.circle(frame, coord, radius, color, thickness)
@@ -123,7 +198,7 @@ class Detector:
                 cv2.putText(frame, f"{self.timer_count - self.time_elapsed}", num_coord, font, num_scale, txt_color,
                             thickness=txt_thickness)
         # Prompts user to confirm the colors
-        elif self.state == States.DONE_2:
+        elif self.state in (States.DONE_1, States.DONE_2):
             prompt = "Press Space to Confirm, Esc to Redo"
             origin = (int(self.width * 0.19), int(self.height * 0.085714))
             cv2.putText(frame, prompt, origin, font, 0.8, (0, 0, 0), thickness=4)
@@ -134,9 +209,9 @@ class Detector:
             if self.state in (States.DONE_1, States.COUNTDOWN_2, States.DONE_2):
                 for track_color, coord in zip(self.colors, self.circle_coordinates):
                     cv2.circle(frame, tuple(coord), radius - 1, track_color, -1)
-        return frame
+        self.processed = frame
 
-    def impose_blocks(self) -> np.array:
+    def draw_drums(self) -> np.array:
         """
         Draws bounding blocks on the bottom of the screen as different drum components
 
@@ -144,47 +219,53 @@ class Detector:
             Output frame
         """
 
-        colors = np.array([[255, 0, 0],
-                           [0, 255, 0],
-                           [0, 0, 255],
-                           [125, 125, 125]])
-        height_factor = 3.5
+        top_drum_y = int(0.875 * self.height)
+        drum_width = self.width // 4
         frame = self.frame.copy()
+        cv2.line(frame, (0, int(self.speed_thresh)), (self.width, int(self.speed_thresh)), (255, 0, 0), 2)
+        cv2.line(frame, (0, int(self.reset_thresh)), (self.width, int(self.reset_thresh)), (255, 0, 0), 2)
+        cv2.line(frame, (0, int(self.height_thresh)), (self.width, int(self.height_thresh)), (255, 0, 0), 2)
         for i in range(4):
-            scale_factor = 1.0 if float(time.time() - self.boxes[i].last_collision) < 0.2 else 0.7
-            new_val = (frame[int((self.height // 4) * height_factor):,
-                       (self.width // 4) * i:(self.width // 4) * (i + 1), :]
-                       + self.boxes[i].color * scale_factor) / 2
-            frame[int((self.height // 4) * height_factor):, (self.width // 4) * i:(self.width // 4) * (i + 1),
-            :] = new_val
+            color_scale = 1.0 if float(time.time() - self.boxes[i].last_collision) < 0.2 else 0.7
+            new_val = (frame[top_drum_y:, drum_width * i:drum_width * (i + 1), :] + self.boxes[
+                i].color * color_scale) / 2
+            frame[top_drum_y:, drum_width * i:drum_width * (i + 1), :] = new_val
         frame[frame > 255] = 255
-        return frame
+        self.processed = frame
 
-    def generate_mask(self, gray: np.array) -> np.array:
+    def _generate_mask(self) -> np.array:
         """
         Uses motion estimation then color thresholding to create a mask to be used for contour detection
-        Args:
-            gray: Grayscale image used for calculating the motion mask
 
         Returns:
             Output mask
         """
         if self.frame_queue:
             # Motion estimation
-            delta = cv2.absdiff(self.frame_queue[0], gray)
-            thresh = cv2.threshold(delta, 20, 255, cv2.THRESH_BINARY)[1]
-            thresh = cv2.dilate(thresh, np.ones((10, 10)), iterations=3)
-            thresh = cv2.bitwise_and(self.frame, self.frame, mask=thresh)
-            cv2.imshow("motion", thresh)
+            delta = cv2.absdiff(self.frame_queue[0], self.gray)
+            movement_mask = cv2.threshold(delta, 20, 255, cv2.THRESH_BINARY)[1]
+            movement_mask = cv2.dilate(movement_mask, np.ones((15, 15)), iterations=1)
+            movement_output = cv2.bitwise_and(self.frame, self.frame, mask=movement_mask)
+            cv2.imshow("motion", movement_output)
             # Color range
-            buffer = 20
-            master_mask = np.zeros(thresh.shape[:2], dtype="uint8")
+            buffer = 35
+            master_mask = np.zeros(movement_output.shape[:2], dtype="uint8")
+
             # Color thresholding
             for color in self.colors:
-                mask = cv2.inRange(thresh, (color - buffer).astype(int), (color + buffer).astype(int))
+                primary_index = np.argmax(color)
+                bottom_range = color - buffer
+                bottom_range[primary_index] += 20
+                top_range = color + buffer
+                top_range[primary_index] -= 20
+
+                mask = cv2.inRange(movement_output, bottom_range.astype(int), top_range.astype(int))
                 master_mask = cv2.bitwise_or(mask, master_mask)
             # Fills the mask for easier detection
-            master_mask = cv2.dilate(master_mask, np.ones((12, 12)), iterations=3)
+            cv2.imshow("pre", master_mask)
+            master_mask = cv2.erode(master_mask, np.ones((3, 3)), iterations=1)
+            cv2.imshow("after-erosion", master_mask)
+            master_mask = cv2.dilate(master_mask, np.ones((9, 9)), iterations=5)
             return master_mask
         else:
             return np.zeros((self.width, self.height), dtype="uint8")
@@ -202,36 +283,35 @@ class Detector:
                 self.state = States.DONE_1
             elif self.state == States.COUNTDOWN_2:
                 self.state = States.DONE_2
-            self.get_color()
+            self.measure_color()
 
-    def generate_contours(self, gray: np.array) -> List:
+    def generate_contours(self) -> List:
         """
         Generates a masked image with the generate_mask method and then computes the contours based on that and returns
         it
 
-        Args:
-            gray: Grayscale 'raw' image
-
         Returns:
             list of contours
         """
-        mask = self.generate_mask(gray)
+        mask = self._generate_mask()
         contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         contours = imutils.grab_contours(contours)
         drawn_contours = self.frame.copy()
-        for c in contours:
-            if cv2.contourArea(c) < 200:
-                continue
+        contours = sorted(contours, key=lambda contour: cv2.contourArea(contour))
+        for c in contours[-3:]:
             M = cv2.moments(c)
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
-
+            cv2.circle(drawn_contours, (cX, cY), 2, (0, 0, 255), 1)
+            top_point = c[c[:, :, 1].argmin()][0, 1]
             for i in range(1, 5):
                 if (self.width // 4) * (i - 1) < cX < (self.width // 4) * i:
                     if cY > self.height_thresh:
                         self.boxes[i - 1].has_collision()
                     else:
                         self.boxes[i - 1].reset_collision()
+                    if top_point < self.speed_thresh:
+                        self.boxes[i - 1].start_timer()
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(drawn_contours, (x, y), (x + w, y + h), (0, 255, 0), 2)
         cv2.imshow("mask", mask)
@@ -255,40 +335,43 @@ class Detector:
             _, frame = cap.read()
             frame = cv2.resize(frame, (700, 700))
             self.frame = cv2.flip(frame, 1)
-            processed = self.impose_blocks()
+            self.draw_drums()
             # Grayscale converted image of 'raw' image
-            gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+            self.gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
             # If color is confirmed, perform masking and contour detection
-            if self.state == States.CONFIRM:
-                contours = self.generate_contours(gray)
-                with open("colors.pickle", "wb") as f:
-                    pickle.dump(self.colors, f)
+            if self.state == States.CONFIRM_2:
+                contours = self.generate_contours()
             # Otherwise prompt the user to calibrate
             else:
-                processed = self.prompt_color()
+                self.prompt_color()
             c = cv2.waitKey(1)
             if c == ord("q"):
                 break
             # If space is pressed and the user is prompted to calibrate
             elif c == ord(" ") and self.state not in (
-                    States.COUNTDOWN_1, States.COUNTDOWN_2, States.CONFIRM, States.DONE_2):
+                    States.COUNTDOWN_1, States.COUNTDOWN_2, States.CONFIRM_2, States.DONE_2):
                 if self.state == States.INIT:
                     self.state = States.COUNTDOWN_1
                 elif self.state == States.DONE_1:
                     self.state = States.COUNTDOWN_2
                 self.start_time = time.time()
             # If space is pressed and the user is prompted to confirm
-            elif c == ord(" ") and self.state == States.DONE_2:
-                self.state = States.CONFIRM
+            elif c == ord(" ") and self.state in (States.DONE_1, States.DONE_2):
+                if self.state == States.DONE_1:
+                    self.state = States.CONFIRM_1
+                else:
+                    self.state = States.CONFIRM_2
+                with open("colors.pickle", "wb") as f:
+                    pickle.dump(self.colors, f)
             # If escape is pressed and the user is prompted to confirm
-            elif c == 27 and self.state == States.DONE_2:
+            elif c == 27 and self.state in (States.DONE_1, States.DONE_2):
                 self.state = States.INIT
                 self.colors.clear()
             # Populates frame queue for motion estimation
-            self.frame_queue.append(gray.copy())
+            self.frame_queue.append(self.gray.copy())
             if len(self.frame_queue) > 3:
                 self.frame_queue.pop(0)
-            cv2.imshow('Input', processed)
+            cv2.imshow('Input', self.processed)
 
         cap.release()
         cv2.destroyAllWindows()
